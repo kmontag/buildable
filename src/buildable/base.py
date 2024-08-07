2a5e4968665b8ca207361341e50598b283b1b362
Kevin Montag <kmontag@cs.stanford.edu>
2024-08-07 05:27:39 +0200

    initial implementation for copying tracks between sets

--- src/buildable/base.py
+++ src/buildable/base.py
@@ -1,12 +1,98 @@
 from __future__ import annotations
 
+import abc
 import gzip
-from typing import TYPE_CHECKING
-from xml.etree.ElementTree import Element, ElementTree
+import json
+from typing import TYPE_CHECKING, Protocol, TypeVar
+
+from lxml.etree import ElementTree, tostring
 
 if TYPE_CHECKING:
     import os
-    from typing import BinaryIO, Final, Self
+    from typing import Any, BinaryIO, Callable, Final, Self
+
+    from lxml.etree import _Element
+
+_E = TypeVar("_E", bound="ElementObject")
+_E2 = TypeVar("_E2", bound="ElementObject")
+_T = TypeVar("_T")
+_T2 = TypeVar("_T2", covariant=True)
+_V = TypeVar("_V", str, int, bool, float)
+
+
+# Generic `property` protocol with the same get/set type. The type checker is able to infer the get/set type hints when
+# this is returned from a decorator. See e.g. https://github.com/python/typing/issues/985.
+class GenericProperty(Protocol[_T2]):
+    def __get__(self, obj: Any, type: type | None = ...) -> _T2:  # noqa: A002
+        ...
+
+
+class GenericMutableProperty(Protocol[_T]):
+    def __get__(self, obj: Any, type: type | None = ...) -> _T:  # noqa: A002
+        ...
+
+    def __set__(self, obj: Any, value: _T) -> None: ...
+
+    def __delete__(self, obj: Any) -> None: ...
+
+
+def xml_property(
+    attrib: str,
+    property_type: "type[_V]",
+) -> Callable[[Callable[[_T], "_Element"]], GenericMutableProperty[_V]]:
+    def inner(fn: Callable[[_T], "_Element"]) -> GenericMutableProperty[_V]:
+        """Define a property which takes its value from an element in an Ableton XML document."""
+
+        def getter(instance: _T) -> _V:
+            element = fn(instance)
+            value = element.attrib.get(attrib, None)
+            if value is None:
+                msg = f"Tag '{element.tag}' has no value"
+                raise ValueError(msg)
+            if not isinstance(value, str):
+                msg = f"Unexpected property type in XML document: {type(value)} (expected str)"
+                raise AssertionError(msg)
+
+            # Special handling for boolean values, which show up as "true"/"false" strings.
+            if issubclass(property_type, bool):
+                if value.lower() in ("true", "false"):
+                    return property_type(value.lower() == "true")
+                else:
+                    msg = f"Unexpected boolean value in XML document: {value}"
+                    raise ValueError(msg)
+
+            return property_type(value)
+
+        def setter(instance: _T, value: _V) -> None:
+            element = fn(instance)
+            # Non-string values should use their JSON string representation.
+            element.attrib[attrib] = value if isinstance(value, str) else json.dumps(value)
+
+        return property(fget=getter, fset=setter)
+
+    return inner
+
+
+def child_element_object_property(
+    property_type: type[_E2],
+) -> Callable[[Callable[[_E], "_Element"]], GenericProperty[_E2]]:
+    def inner(fn: Callable[[_E], "_Element"]) -> GenericProperty[_E2]:
+        def getter(instance: _E) -> _E2:
+            element: "_Element" = fn(instance)
+            child_elements = element.findall(property_type.TAG)
+            if len(child_elements) == 0:
+                msg = f"Child element of type '{property_type.TAG}' not found"
+                raise ValueError(msg)
+
+            if len(child_elements) > 1:
+                msg = f"Found more than one child element of type '{property_type.TAG}'"
+                raise ValueError(msg)
+
+            return property_type(child_elements[0])
+
+        return property(fget=getter)
+
+    return inner
 
 
 class AbletonDocumentObject:
@@ -31,10 +117,10 @@ class AbletonDocumentObject:
         if len(root) != 1:
             msg = "The data must contain exactly one nested element"
             raise ValueError(msg)
-        self._element: Element = root[0]
+        self._element: "_Element" = root[0]
 
     @property
-    def element(self) -> Element:
+    def element(self) -> "_Element":
         """The XML element representing the document's primary object."""
         return self._element
 
@@ -48,7 +134,25 @@ class AbletonDocumentObject:
             # Output the XML prolog manually, so we can match the exact formatting for native Ableton files.
             gzipped_output.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
 
-            self._element_tree.write(gzipped_output, xml_declaration=False, encoding="utf-8", method="xml")
+            # Pretty-print the main tree.
+            pretty_xml_str = tostring(self._element_tree, pretty_print=True, xml_declaration=False, encoding="utf-8")
 
-            # Output a trailing newline to match native files.
-            gzipped_output.write(b"\n")
+            # Live documents add an extra space to self-closing tags.
+            pretty_xml_str = pretty_xml_str.replace(b"/>\n", b" />\n")
+
+            # Write the pretty-printed content.
+            gzipped_output.write(pretty_xml_str)
+
+
+class ElementObject(abc.ABC):  # noqa: B024
+    TAG: str = NotImplemented
+
+    def __init__(self, element: "_Element"):
+        if element.tag != self.TAG:
+            msg = f"Expected element of type '{self.TAG}', but got '{element.tag}'"
+            raise ValueError(msg)
+        self._element = element
+
+    @property
+    def element(self) -> "_Element":
+        return self._element
